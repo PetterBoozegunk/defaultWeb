5cf61dbf34aea311b298ea68b4329f4261c71bfa
Petter Ahlberg <petter.ahlberg@gmail.com>
2015-03-10 13:50:33 +0100

    Gulpfile works

--- UI/gulpfile.js
+++ UI/gulpfile.js
@@ -2,74 +2,104 @@
 "use strict";
 
 var gulp = require("gulp"),
+    watch = require("gulp-watch"),
     concat = require("gulp-concat"),
-    del = require("del"),
-    //path = require("path"),
-    //cssGlobbing = require("gulp-css-globbing"),
     less = require("gulp-less"),
     minifyCSS = require("gulp-minify-css"),
     LessPluginAutoPrefix = require("less-plugin-autoprefix"),
     autoprefix = new LessPluginAutoPrefix({
         browsers: ["last 4 versions"]
     }),
-    //sourcemaps = require("gulp-sourcemaps"),
     uglify = require("gulp-uglify"),
     includeSources = require("gulp-include-source"),
 
-    srcDest = "dist",
-    mapsDest = "./maps",
+    gulpSettings = {
 
-    util = {
-        "del" : function (dest) {
-            del([dest], function (err, deletedFiles) {
-                console.log("Files deleted:", deletedFiles.join(', '));
+        srcDest: "dist",
+        cssDest: "/css",
+        jsDest: "/js",
+
+        tasks: {
+            "less": function () {
+                return gulp.src([
+                    "less/fonts/*",
+                    "less/vars/*",
+                    "less/mixins/*",
+                    "less/default/*",
+                    "less/grid/*",
+                    "less/site/*",
+                    "less/pages/*",
+                    "less/components/*"
+                ])
+                .pipe(concat("styles.less"))
+                .pipe(less({
+                    plugins: [autoprefix]
+                }))
+                .pipe(minifyCSS())
+                .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.cssDest));
+            },
+            "js": function () {
+                return gulp.src(["js/lib/*.js", "js/*.js"])
+                    .pipe(concat("scripts.js"))
+                    .pipe(uglify())
+                    .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.jsDest));
+            },
+            "odlIeCss" : function () {
+                return gulp.src("less/oldIe/*")
+                .pipe(concat("oldIe.less"))
+                .pipe(less({
+                    plugins: [autoprefix]
+                }))
+                .pipe(minifyCSS())
+                .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.cssDest));
+            },
+            "oldIeJs" : function () {
+                return gulp.src("js/oldIe/*.js")
+                    .pipe(concat("oldIe.js"))
+                    .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.jsDest));
+            },
+            "tests": function () {
+                return gulp.src(["js/tests/*.js"])
+                    .pipe(concat("tests.js"))
+                    .pipe(uglify())
+                    .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.jsDest));
+            },
+            "watch" : function () {
+                gulp.watch("js/lib/**.js", ["js"]);
+                gulp.watch("js/**.js", ["js", "oldIeJs", "tests"]);
+
+                gulp.watch("less/**/*.less", ["less", "odlIeCss"]);
+            }
+        },
+        eachTask : function (func, that) {
+            var k,
+                tasks = gulpSettings.tasks;
+
+            for (k in tasks) {
+                if (tasks.hasOwnProperty(k)) {
+                    func.call(that, tasks[k], k, tasks);
+                }
+            }
+        },
+        getAllTasks: function () {
+            var tasksArray = [];
+
+            gulpSettings.eachTask(function () {
+                tasksArray.push(arguments[1]);
+            });
+
+            return tasksArray;
+        },
+        setAllTasks : function () {
+            gulpSettings.eachTask(function () {
+                var taskName = arguments[1],
+                    taskFunc = arguments[0];
+
+                gulp.task(taskName, taskFunc);
             });
         }
-    }
+    },
+    tasksArray = gulpSettings.getAllTasks();
 
-gulp.task("less", function () {
-    //util.del(srcDest + "/css/styles.css");
-
-    return gulp.src([
-        "less/fonts/*",
-        "less/vars/*",
-        "less/mixins/*",
-        "less/default/*",
-        "less/grid/*",
-        "less/site/*",
-        "less/pages/*",
-        "less/components/*"
-    ])
-    .pipe(concat("styles.less"))
-    .pipe(less({
-        plugins: [autoprefix]
-    }))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest(srcDest + "/css"));
-});
-
-gulp.task("js", function () {
-    //util.del(srcDest + "/js/scripts.js");
-
-    return gulp.src(["js/lib/*.js", "js/*.js"])
-        .pipe(concat("scripts.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest(srcDest + "/js"));
-});
-
-gulp.task("tests", function () {
-    //util.del(srcDest + "/js/tests.js");
-
-    return gulp.src(["js/tests/*.js"])
-        .pipe(concat("tests.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest(srcDest + "/js"));
-});
-
-gulp.task("html", function () {
-    return gulp.src(["../mod_blocks/styles.html", "../mod_blocks/scripts.html"])
-      .pipe(includeSources())
-      .pipe(gulp.dest("../blocks"));
-});
-
-gulp.task("default", ["less", "js", "tests", "html"]);
+gulpSettings.setAllTasks();
+gulp.task("default", tasksArray);
