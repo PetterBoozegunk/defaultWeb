c994d8b300d27a7dc40746f89d7cc33662d87fc2
Petter Ahlberg <petter.ahlberg@gmail.com>
2015-04-16 21:48:44 +0200

    Gulp fine-tuning

--- UI/gulpfile.js
+++ UI/gulpfile.js
@@ -1,4 +1,5 @@
-﻿/*jslint node: true */
+﻿/*global require */
+/*jslint node: true */
 "use strict";
 
 var gulp = require("gulp"),
@@ -8,12 +9,16 @@ var gulp = require("gulp"),
     lessPluginGlob = require("less-plugin-glob"),
     stripCssComments = require("gulp-strip-css-comments"),
     less = require("gulp-less"),
+
     please = require("gulp-pleeease"),
+    minifyCSS = require("gulp-minify-css"),
 
     jslint = require("gulp-jslint"),
     uglify = require("gulp-uglify"),
 
     imagemin = require("gulp-imagemin"),
+
+    sourcemaps = require("gulp-sourcemaps"),
 
     util = {
         forEach: function (func, that, obj) {
@@ -26,10 +31,7 @@ var gulp = require("gulp"),
             }
         },
         setGulp: function (type, obj) {
-            util.forEach(function () {
-                var name = arguments[1],
-                    funcArray = arguments[0];
-
+            util.forEach(function (funcArray, name) {
                 gulp[type](name, funcArray);
             }, this, obj);
         }
@@ -40,64 +42,90 @@ var gulp = require("gulp"),
         srcDest: "dist",
         cssDest: "/css",
         jsDest: "/js",
-        sourceMapDest: ".",
 
-        less : {
+        lessSrc: ["less/styles.less"],
+        less: {
             "plugins": [lessPluginGlob]
         },
         comments: {
             all: true
         },
-        please : {
+        please: {
             "browsers": ["last 4 versions"],
             "minifier": false,
-            "sourcemaps": false,
+            "autoprefixer": {
+                "cascade": false
+            },
             "filters": {
                 "oldIE": true
             }
         },
 
-        watch : {
-            "js/lib/**.js": ["scripts:main"],
-            "js/**.js": ["scripts:main", "scripts:ie", "tests"],
-            "less/**": ["less:main", "less:ie"]
+        watch: {
+            "js/lib/**.js": ["scripts:dev"],
+            "js/**.js": ["scripts:dev"],
+            "js/oldIe/**.js": ["scripts:ie:all"],
+            "less/**": ["less:dev"],
+            "less/oldIe/**": ["less:ie:dev"]
         },
 
         tasks: {
-            "less:main": function () {
-                return gulp.src("less/styles.less")
+            "watch": function () {
+                util.setGulp("watch", gulpSettings.watch);
+            },
+            "less:prod": function () {
+                return gulp.src(gulpSettings.lessSrc)
                     .pipe(less(gulpSettings.less))
                     .pipe(please(gulpSettings.please))
+                    .pipe(minifyCSS())
                     .pipe(stripCssComments(gulpSettings.comments))
                     .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.cssDest));
             },
-            "less:ie" : function () {
-                return gulp.src("less/oldIe/*")
+            "less:dev": function () {
+                return gulp.src(gulpSettings.lessSrc)
+                    .pipe(sourcemaps.init())
+                    .pipe(less(gulpSettings.less))
+                    .pipe(please(gulpSettings.please))
+                    .pipe(stripCssComments(gulpSettings.comments))
+                    .pipe(sourcemaps.write("."))
+                    .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.cssDest));
+            },
+            "less:ie:prod": function () {
+                return gulp.src("less/oldIe/*.less")
                     .pipe(concat("oldIe.less"))
                     .pipe(less(gulpSettings.less))
                     .pipe(please(gulpSettings.please))
+                    .pipe(minifyCSS())
                     .pipe(stripCssComments(gulpSettings.comments))
                     .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.cssDest));
             },
-            "jslint" : function () {
-                return gulp.src(["!js/lib", "js/*.js"])
+            "less:ie:dev": function () {
+                return gulp.src("less/oldIe/*.less")
+                    .pipe(concat("oldIe.less"))
+                    .pipe(less(gulpSettings.less))
+                    .pipe(please(gulpSettings.please))
+                    .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.cssDest));
+            },
+            "jslint": function () {
+                return gulp.src(["!js/lib", "js/*.js", "gulpfile.js"])
                     .pipe(jslint());
             },
-            "scripts:main": function () {
+            "scripts:prod": function () {
                 return gulp.src(["js/lib/*.js", "js/*.js"])
                     .pipe(concat("scripts.js"))
                     .pipe(uglify())
                     .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.jsDest));
             },
-            "scripts:ie" : function () {
-                return gulp.src("js/oldIe/*.js")
-                    .pipe(concat("oldIe.js"))
+            "scripts:dev": function () {
+                return gulp.src(["js/lib/*.js", "js/*.js"])
+                    .pipe(sourcemaps.init())
+                    .pipe(concat("scripts.js"))
+                    .pipe(sourcemaps.write("."))
                     .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.jsDest));
             },
-            "tests": function () {
-                return gulp.src(["js/tests/*.js"])
-                    .pipe(concat("tests.js"))
-                    .pipe(uglify())
+            "scripts:ie:all": function () {
+                return gulp.src("js/oldIe/*.js")
+                    .pipe(concat("oldIe.js"))
                     .pipe(gulp.dest(gulpSettings.srcDest + gulpSettings.jsDest));
             },
             "images": function () {
@@ -105,10 +133,8 @@ var gulp = require("gulp"),
                     .pipe(imagemin())
                     .pipe(gulp.dest("images/"));
             },
-            "watch": function () {
-                util.setGulp("watch", gulpSettings.watch);
-            },
-            "default": ["less:main", "less:ie", "jslint", "scripts:main", "scripts:ie", "tests", "images", "watch"]
+            "default": ["less:dev", "less:ie:dev", "jslint", "scripts:dev", "scripts:ie:all", "images"],
+            "prod": ["less:prod", "less:ie:prod", "scripts:prod", "scripts:ie:all"]
         }
     };
 
