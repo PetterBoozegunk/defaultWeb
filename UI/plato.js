258d843186ec2694e855c3914cba549bfc911252
Petter Ahlberg <petter.ahlberg@gmail.com>
2015-10-19 10:40:08 +0200

    Plato changes

--- UI/plato.js
+++ UI/plato.js
@@ -3,7 +3,6 @@
 "use strict";
 
 var plato = require("plato"),
-    chalk = require("chalk"),
 
     gulp = require("gulp"),
     gutil = require("gulp-util"),
@@ -13,11 +12,33 @@ var plato = require("plato"),
     // null options for this example
     options = {},
 
+    config = {
+        "maintanability-min": 75,
+        "loc-max": 6 // loc = Lines Of Code
+    },
+
     twoDigits = function (digit) {
         return (digit.toString().length < 2) ? "0" + digit : digit;
     },
-    callback = function () {
-        gutil.log("Plato report done");
+    getColor = function (complexity) {
+        var color = "green",
+            maintainability = complexity.maintainability,
+            loc = complexity.loc;
+
+        if (maintainability < config["maintanability-min"] || loc > config["loc-max"]) {
+            color = "red";
+        }
+
+
+        return color;
+    },
+    callback = function (report) {
+        Object.keys(report).forEach(function (item) {
+            var itemObj = report[item],
+                color = getColor(itemObj.complexity);
+
+            gutil.log(gutil.colors[color](itemObj.info.file));
+        });
     };
 
 plato.inspect(files, outputDir, options, callback);
