1a42bdb82bc387e5161b27d3bd5b64a533b7ae4e
Petter Ahlberg <petter.ahlberg@gmail.com>
2015-10-20 08:54:08 +0200

    Refactor plato.js

--- UI/plato.js
+++ UI/plato.js
@@ -7,42 +7,99 @@ var plato = require("plato"),
     gulp = require("gulp"),
     gutil = require("gulp-util"),
 
-    files = ["*.js", "js/plugins/*.js", "js/*.js", "js/tests/*.js", "../*.js"],
-    outputDir = "./report",
-    // null options for this example
-    options = {},
+    p = {
+        files: ["*.js", "js/plugins/*.js", "js/*.js", "js/tests/*.js", "../*.js"],
+        outputDir: "./report",
+        // null options for this example
+        options: {},
 
-    config = {
-        "maintanability-min": 75,
-        "loc-max": 6 // loc = Lines Of Code
-    },
+        config: {
+            "maintainability-min": 75,
+            "loc-max": 6 // loc = Lines Of Code
+        },
+        pass: {},
 
-    twoDigits = function (digit) {
-        return (digit.toString().length < 2) ? "0" + digit : digit;
-    },
-    getColor = function (complexity) {
-        var maintainability = complexity.maintainability,
-            loc = complexity.loc,
-            color = (maintainability < config["maintanability-min"] || loc > config["loc-max"]) ? "red" : "green";
+        getColor: function (bool) {
+            return (bool === true) ? "green" : "red";
+        },
 
-        return color;
-    },
-    startLog = function () {
-        console.log(" ");
-        gutil.log(gutil.colors.magenta("node plato..."));
-    },
-    logReport = function (report) {
-        Object.keys(report).forEach(function (item) {
-            var itemObj = report[item],
-                color = getColor(itemObj.complexity),
-                reportStr = "maintainability: " + itemObj.complexity.maintainability.toFixed(2) + ", loc: " + itemObj.complexity.loc.toFixed(2);
+        checkMinMax: function (type, minMax) {
+            return p.config[type + "-" + minMax] ? minMax : "";
+        },
+        getMinMax: function (type) {
+            var max = p.checkMinMax(type, "max"),
+                min = p.checkMinMax(type, "min");
 
-            gutil.log(gutil.colors[color](itemObj.info.file), gutil.colors.grey(reportStr));
-        });
-    },
-    callback = function (report) {
-        startLog();
-        logReport(report);
+            return max || min;
+        },
+        maxCheck: function (configValue, value) {
+            return (value <= configValue);
+        },
+        minCheck: function (configValue, value) {
+            return (value >= configValue);
+        },
+        setFilePass: function (fileName, pass) {
+            if (p.pass[fileName] === undefined || p.pass[fileName] === true) {
+                p.pass[fileName] = pass;
+            }
+        },
+        checkPass: function (configValue, minMax, value, fileName) {
+            var pass = configValue ? p[minMax + "Check"](configValue, value) : false;
+
+            p.setFilePass(fileName, pass);
+
+            return pass;
+        },
+        setDigitColor: function (name, itemObj) {
+            var value = itemObj.complexity[name],
+                minMax = p.getMinMax(name),
+                configValue = p.config[name + "-" + minMax],
+                pass = p.checkPass(configValue, minMax, value, itemObj.info.file),
+                color = p.getColor(pass);
+
+            return gutil.colors[color](value.toFixed(2));
+        },
+        getCheckArray: function (checkArray) {
+            Object.keys(p.config).forEach(function (fullName) {
+                var name = fullName.replace(/\-(min|max)/, "");
+
+                checkArray.push(name);
+            });
+
+            return checkArray;
+        },
+        getLogArray: function (itemObj, logArray) {
+            var checkArray = p.getCheckArray([]);
+
+            checkArray.forEach(function (name) {
+                logArray.push("\n\t\t" + gutil.colors.grey(name) + ":");
+                logArray.push(p.setDigitColor(name, itemObj));
+            });
+
+            return logArray;
+        },
+        logReport: function (report) {
+            Object.keys(report).forEach(function (item) {
+                var itemObj = report[item],
+                    logArray = p.getLogArray(itemObj, []),
+                    color = p.getColor(p.pass[itemObj.info.file]);
+
+                logArray.splice(0, 0, gutil.colors[color](itemObj.info.file));
+
+                gutil.log.apply(undefined, logArray);
+            });
+        },
+        startLog: function () {
+            //console.log(" ");
+            gutil.log(gutil.colors.magenta("node plato..."));
+        },
+        callback: function (report) {
+            p.startLog();
+            p.logReport(report);
+        },
+        init: function () {
+            plato.inspect(p.files, p.outputDir, p.options, p.callback);
+        }
     };
 
-plato.inspect(files, outputDir, options, callback);
+p.init();
