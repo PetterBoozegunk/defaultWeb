2343acb8817a4a26ac6d091b1debd2d4587ec102
Petter Ahlberg <petter.ahlberg@gmail.com>
2015-11-06 11:02:20 +0100

    Browsersync bug
    Browsersync did not work with gzip

--- UI/gulp/tasks/browsersync-watch.js
+++ UI/gulp/tasks/browsersync-watch.js
@@ -14,10 +14,10 @@ var gulp = require("gulp"),
     settings = {
         options: {
             proxy: {
-                target: config.developerRoot,
-                proxyRes: [function (res) {
-                    res.headers["content-encoding"] = "gzip";
-                }]
+                target: config.developerRoot //,
+                //proxyRes: [function (res) {
+                //    res.headers["content-encoding"] = "gzip";
+                //}]
             },
             browser: ["firefox"],
             reloadDelay: 500
