28da0f4cb5a3da8ee871122f138db0f5e8119f7f
Petter Ahlberg <petter.ahlberg@gmail.com>
2016-02-05 08:33:13 +0100

    Add gulp-remove-empty-lines

--- UI/gulp/tasks/less.js
+++ UI/gulp/tasks/less.js
@@ -34,7 +34,7 @@ var gulp = require("gulp"),
     },
 
     // There just must be a better way to get jsbeautifier + jslint to handle this better... 
-    mainLessPipe = lazypipe().pipe(plugins.less, settings.options).pipe(plugins.shorthand).pipe(plugins.pleeease, settings.pleeease).pipe(plugins.stripCssComments, settings.comments),
+    mainLessPipe = lazypipe().pipe(plugins.less, settings.options).pipe(plugins.shorthand).pipe(plugins.pleeease, settings.pleeease).pipe(plugins.stripCssComments, settings.comments).pipe(plugins.removeEmptyLines),
 
     less = {
         tasks: {
